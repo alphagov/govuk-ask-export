d6fc3aeffb7afabf1454434bd1113e915c765433
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2020-12-29 16:34:31 +0000

    Add pipeline class and config
    This introduces a Pipeline class that represents a transformation and
    how the data is exported from Smart Survey. There will be a pipeline for
    each file that is currently generated. This will be used to abstract
    away the different files that need to generated and exported. This is to
    remove hard coding configuration in code and the tests.

---
+++ lib/ask_export/pipeline.rb
@@ -0,0 +1,18 @@
+module AskExport
+  class Pipeline
+    attr_reader :name, :fields, :only_completed
+
+    def self.load_all(config_path)
+      pipelines = YAML.load_file(config_path).fetch("pipelines")
+      pipelines.map do |name, config|
+        new(name: name, **config.transform_keys(&:to_sym))
+      end
+    end
+
+    def initialize(name:, fields: [], only_completed: true)
+      @name = name
+      @fields = fields.map(&:to_sym)
+      @only_completed = only_completed
+    end
+  end
+end
