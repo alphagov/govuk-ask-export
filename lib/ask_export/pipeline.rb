4ea14da8d9767e28c5c4f8e8f2fbdd007eddafab
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2021-01-03 12:01:19 +0000

    Add destinations to pipeline class
    This allows pipelines to express export destinations such as Google
    Drive or local filesystem. This will be used to build a more extensible
    way to write new exporters.

--- lib/ask_export/pipeline.rb
+++ lib/ask_export/pipeline.rb
@@ -1,6 +1,6 @@
 module AskExport
   class Pipeline
-    attr_reader :name, :fields, :only_completed
+    attr_reader :name, :fields, :only_completed, :destinations
 
     def self.load_all(config_path)
       pipelines = YAML.load_file(config_path).fetch("pipelines")
@@ -9,10 +9,11 @@ module AskExport
       end
     end
 
-    def initialize(name:, fields: [], only_completed: true)
+    def initialize(name:, fields: [], only_completed: true, destinations: [])
       @name = name
       @fields = fields.map(&:to_sym)
       @only_completed = only_completed
+      @destinations = destinations
     end
   end
 end
--- spec/ask_export/pipeline_spec.rb
+++ spec/ask_export/pipeline_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe AskExport::Pipeline do
       expect(pipeline.name).to eq("pipeline1")
       expect(pipeline.fields).to eq([])
       expect(pipeline.only_completed).to be(true)
+      expect(pipeline.destinations).to eq([])
     end
 
     it "returns object with fields as symbols" do
@@ -27,6 +28,12 @@ RSpec.describe AskExport::Pipeline do
       pipeline = AskExport::Pipeline.new(name: "pipeline1", only_completed: false)
 
       expect(pipeline.only_completed).to be(false)
+    end
+
+    it "returns object with destinations" do
+      pipeline = AskExport::Pipeline.new(name: "pipeline1", destinations: %w[a b c])
+
+      expect(pipeline.destinations).to eq(%w[a b c])
     end
   end
 end
